e6f1eabd9653614eeb00d19f27748d430eec2f09
Karen Araqelyan <karen.araqelyan@hylink.am>
2025-09-05 15:34:45 +0400

    add dob autocomplate field implementation

--- test/dob_autocomplete_field_test.dart
+++ test/dob_autocomplete_field_test.dart
@@ -1,12 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:dob_autocomplete_field/dob_autocomplete_field.dart';
+// import 'package:dob_autocomplete_field/dob_autocomplete_field.dart';
 
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+
   });
 }
